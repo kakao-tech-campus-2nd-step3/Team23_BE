6915ed541f7b903955d75dc3eff6db8f84946c40
minju26 <mjalswn26@gmail.com>
2024-09-24 21:41:25 +0900

    Feat: common api response 정의
    - 사용 예시를 위한 example controller 포함

---
+++ src/main/java/kappzzang/jeongsan/common/JeongsanApiResponse.java
@@ -0,0 +1,43 @@
+package kappzzang.jeongsan.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class JeongsanApiResponse<T> {
+
+    private final String status;
+    private final int httpCode;
+    private final String errorCode;
+    private final String message;
+    private final T data;
+
+    public static <T> JeongsanApiResponse<T> success(SuccessType successType) {
+        return JeongsanApiResponse.<T>builder()
+                .status("success")
+                .httpCode(successType.getHttpStatusCode())
+                .message(successType.getMessage())
+                .build();
+    }
+
+    public static <T> JeongsanApiResponse<T> success(SuccessType successType, T data) {
+        return JeongsanApiResponse.<T>builder()
+                .status("success")
+                .httpCode(successType.getHttpStatusCode())
+                .message(successType.getMessage())
+                .data(data)
+                .build();
+    }
+
+    public static JeongsanApiResponse<Void> failure(ErrorType errorType) {
+        return JeongsanApiResponse.<Void>builder()
+                .status("failure")
+                .httpCode(errorType.getHttpStatusCode())
+                .errorCode(errorType.getErrorCode())
+                .message(errorType.getMessage())
+                .build();
+    }
+}
---
+++ src/main/java/kappzzang/jeongsan/controller/ExampleController.java
@@ -0,0 +1,31 @@
+package kappzzang.jeongsan.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import kappzzang.jeongsan.common.JeongsanApiResponse;
+import kappzzang.jeongsan.domain.Team;
+import org.springframework.web.bind.annotation.*;
+
+import static kappzzang.jeongsan.common.ErrorType.USER_NOT_FOUND;
+import static kappzzang.jeongsan.common.SuccessType.TEAM_CREATED;
+
+@Hidden
+@RestController
+@RequestMapping("/api/test")
+public class ExampleController {
+
+    @GetMapping("/success")
+    public JeongsanApiResponse<Void> successWithNoData() {
+        return JeongsanApiResponse.success(TEAM_CREATED);
+    }
+
+    @PostMapping("/success/data")
+    public JeongsanApiResponse<Team> successWithData() {
+        return JeongsanApiResponse.success(TEAM_CREATED, new Team());
+    }
+
+    @GetMapping("/failure")
+    @ResponseStatus()
+    public JeongsanApiResponse<Void> failure() {
+        return JeongsanApiResponse.failure(USER_NOT_FOUND);
+    }
+}
