1e6d7e06861343af1554b173a4414ffb200960a0
minju26 <mjalswn26@gmail.com>
2024-09-24 21:39:09 +0900

    Feat: success, error type 정의

---
+++ src/main/java/kappzzang/jeongsan/common/ErrorType.java
@@ -0,0 +1,20 @@
+package kappzzang.jeongsan.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorType {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "000", "사용자를 찾을 수 없습니다.");
+
+    private final int httpStatusCode;
+    private final String errorCode;
+    private final String message;
+
+    ErrorType(int httpStatusCode, String errorCode, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
---
+++ src/main/java/kappzzang/jeongsan/common/SuccessType.java
@@ -0,0 +1,18 @@
+package kappzzang.jeongsan.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum SuccessType {
+
+    TEAM_CREATED(HttpStatus.CREATED.value(), "모임이 생성되었습니다.");
+
+    private final int httpStatusCode;
+    private final String message;
+
+    SuccessType(int httpStatusCode, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.message = message;
+    }
+}
