34b37e9870e3a9fb9840e4e3a5bd2d209a69df38
dkswoals <a4445656@gmail.com>
2024-09-24 21:22:48 +0900

    Feat: ApiClientConfig 구현
    - 외부 api 호출을 위한 restClient 설정(Clova, OpenAi)

---
+++ src/main/java/kappzzang/jeongsan/config/ApiClientConfig.java
@@ -0,0 +1,47 @@
+package kappzzang.jeongsan.config;
+
+import java.time.Duration;
+import kappzzang.jeongsan.common.interceptor.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class ApiClientConfig {
+
+    private final Long apiTimeout;
+    private final ClovaOcrProperties clovaOcrProperties;
+    private final OpenAiProperties openAiProperties;
+
+    public ApiClientConfig(@Value("${external.api.timeout}") Long apiTimeout,
+        ClovaOcrProperties clovaOcrProperties, OpenAiProperties openAiProperties) {
+        this.apiTimeout = apiTimeout;
+        this.clovaOcrProperties = clovaOcrProperties;
+        this.openAiProperties = openAiProperties;
+    }
+
+    public RestClient clovaOcrClient() {
+        return createRestClientBuilder()
+            .defaultHeader("X-OCR-SECRET", clovaOcrProperties.key())
+            .build();
+    }
+
+    public RestClient openAiClient() {
+        return createRestClientBuilder()
+            .baseUrl(openAiProperties.url())
+            .defaultHeader("Authorization", openAiProperties.authType() + openAiProperties.key())
+            .build();
+    }
+
+    private RestClient.Builder createRestClientBuilder() {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+            .withConnectTimeout(Duration.ofMillis(apiTimeout))
+            .withReadTimeout(Duration.ofMillis(apiTimeout));
+
+        return RestClient.builder()
+            .requestFactory(ClientHttpRequestFactories.get(settings))
+            .requestInterceptor(new LoggingInterceptor());
+    }
+}
