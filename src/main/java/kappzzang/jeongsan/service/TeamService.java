c3c9a006b59cf7a7b047333cb941948a4853f22d
us4c0d3 <jwseok312@gmail.com>
2024-09-26 15:06:44 +0900

    Feat: 트랜잭션 어노테이션 추가

--- src/main/java/kappzzang/jeongsan/service/TeamService.java
+++ src/main/java/kappzzang/jeongsan/service/TeamService.java
@@ -3,6 +3,7 @@ package kappzzang.jeongsan.service;
 import kappzzang.jeongsan.dto.response.TeamResponse;
 import kappzzang.jeongsan.repository.TeamRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class TeamService {
         this.teamRepository = teamRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<TeamResponse> getTeamsByIsClosed(Boolean isClosed) {
         return teamRepository.findByIsClosed(isClosed)
                 .stream()
