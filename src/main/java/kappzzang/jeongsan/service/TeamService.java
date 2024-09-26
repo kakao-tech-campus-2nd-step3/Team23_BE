2404ecc03ca0d29ade37097c8029d16d20bcaf9a
us4c0d3 <jwseok312@gmail.com>
2024-09-26 15:07:31 +0900

    Feat: 생성자 Lombok 어노테이션으로 대체

--- src/main/java/kappzzang/jeongsan/service/TeamService.java
+++ src/main/java/kappzzang/jeongsan/service/TeamService.java
@@ -2,19 +2,17 @@ package kappzzang.jeongsan.service;
 
 import kappzzang.jeongsan.dto.response.TeamResponse;
 import kappzzang.jeongsan.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TeamService {
 
     private final TeamRepository teamRepository;
-
-    public TeamService(TeamRepository teamRepository) {
-        this.teamRepository = teamRepository;
-    }
 
     @Transactional(readOnly = true)
     public List<TeamResponse> getTeamsByIsClosed(Boolean isClosed) {
