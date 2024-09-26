240e6565f0a92ef57742cd25bd56661dec6bca47
minju26 <mjalswn26@gmail.com>
2024-09-26 15:33:37 +0900

    Refactor: 예외 발생 시 로깅 처리

--- src/main/java/kappzzang/jeongsan/global/exception/GlobalExceptionHandler.java
+++ src/main/java/kappzzang/jeongsan/global/exception/GlobalExceptionHandler.java
@@ -1,15 +1,17 @@
 package kappzzang.jeongsan.global.exception;
 
 import kappzzang.jeongsan.global.common.JeongsanApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(JeongsanException.class)
     public JeongsanApiResponse<Void> jeongsanExceptionHandler(JeongsanException exception) {
-        exception.printStackTrace();
+        log.error("Jeongsan Exception occurred", exception);
         return JeongsanApiResponse.failure(exception.getErrorType());
     }
 }
