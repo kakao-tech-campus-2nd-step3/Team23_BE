55e1ad2c4c4592388b5abbc82c12f318c910ab9d
minju26 <110724254+minju26@users.noreply.github.com>
2024-09-26 19:26:17 +0900

    Feature/issue#14: custom api response, exception handler 구현 (#19)
    * Chore: PR 템플릿 수정
    
    - 자동으로 연관 이슈 close 되도록 수정
    
    * Fix: AccessLevel에 대한 import 추가
    
    * Feat: success, error type 정의
    
    * Env: Lombok 어노테이션 프로세서 추가
    
    - builder -> cannot find symbol 에러 해결
    
    * Feat: common api response 정의
    
    - 사용 예시를 위한 example controller 포함
    
    * Chore: 패키지 구조 조정
    
    - enumeration 패키지를 common 하위로 이동
    - response 관련 enum 클래스 이동
    
    * Feat: custom exception 및 global exception handler 구현
    
    * Docs: PR 템플릿 수정
    
    - "의논 필요" 추가
    
    * Chore: Gradle caching 임시 주석 처리
    
    * Chore: Gradle caching 임시 주석 해제
    
    * Fix: 베이스 엔티티 접근 제한자 변경
    
    * Chore: ci test 실패 관련 수정
    
    - ci 스크립트 내 캐싱 임시 주석
    - 베이스 엔티티 접근 제한자 수정
    
    * Style: 불필요한 개행 삭제
    
    * Chore: annotationProcessor를 compileOnly에 확장
    
    - CI 테스트 실패 해결 테스트
    
    * Chore: CI 스크립트 수정
    
    - pull_request_target -> pull_request
    
    * Chore: 패키지 구조 조정
    
    * Refactor: 예외 발생 시 로깅 처리
    
    * Chore: rest-assured 추가
    
    * Refactor: 공통 api response 수정
    
    - Response Entity 사용
    
    * Test: 공통 api response 테스트
    
    * Refactor: HttpStatusCode로 변경

---
+++ src/main/java/kappzzang/jeongsan/global/common/enumeration/ErrorType.java
@@ -0,0 +1,21 @@
+package kappzzang.jeongsan.global.common.enumeration;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public enum ErrorType {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "000", "사용자를 찾을 수 없습니다.");
+
+    private final HttpStatusCode httpStatusCode;
+    private final String errorCode;
+    private final String message;
+
+    ErrorType(HttpStatusCode httpStatusCode, String errorCode, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
