e8453794eb09ae22a19875be17da70a1ccb9f637
dkswoals <a4445656@gmail.com>
2024-09-27 03:49:36 +0900

    Feat: ReceiptController 구현
    - mockAnalyzeReceipt는 더미 Api

--- src/main/java/kappzzang/jeongsan/global/common/enumeration/SuccessType.java
+++ src/main/java/kappzzang/jeongsan/global/common/enumeration/SuccessType.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatusCode;
 @Getter
 public enum SuccessType {
 
-    TEAM_CREATED(HttpStatus.CREATED, "모임이 생성되었습니다.");
+    TEAM_CREATED(HttpStatus.CREATED, "모임이 생성되었습니다."),
+    RECEIPT_ANALYSIS_SUCCESS(HttpStatus.OK, "영수증 분석을 성공하였습니다.");
 
     private final HttpStatusCode httpStatusCode;
     private final String message;
