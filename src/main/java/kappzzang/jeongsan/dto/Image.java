e52a6bd8978413d0db3538f121775e47ed879b4b
An Jaemin <a4445656@gmail.com>
2024-09-27 22:35:06 +0900

    Feature/issue#15 영수증 OCR Api 구현 (#22)
    * chore: api-keys 설정 추가
    
    * Feat: LoggingInterceptor 구현
    
    외부 Api 호출 시 요청의 logging을 위함
    
    * Feat: 외부 Api Properties 구현
    
    - Clova OCR, OpenAI Api 호출을 위한 필드
    
    * Fix: Slf4j logging issue 해결
    
    - Slf4j log 사용 중 컴파일 단계에서 "cannot find symbol" 오류 발생
    
    - 해결 -> 직접 Logger 생성
    
    - annotationProcessor 'org.projectlombok:lombok'
      추가 시 에러 미발생 -> 정확한 원인 발견 후 추가할지 논의 필요
    
    * Feat: ApiClientConfig 구현
    
    - 외부 api 호출을 위한 restClient 설정(Clova, OpenAi)
    
    * Feat: generalOCR api Response, Request DTO 구현
    
    * Refactor: DTO 필드 추가 및 순서 수정
    
    * Refactor: 누락된 어노테이션 추가
    
    * Env: Lombok 어노테이션 프로세서 추가
    
    * Chore: 패키지 구조 수정
    
    LoggingInterceptor common -> global
    
    * Env: Retry 의존성 추가
    
    - 외부 Api 에러 발생 시 재시도 로직을 구현하기 위함
    
    * Feat: ApiClientConfig 수정
    
    - Retry 기능을 위해 EnableRetry 어노테이션 추가
    - RestClient가 아닌 RestClient.Builder로 빈 등록 -> restClient Test 유연성 확보
    - 명시적 Qualifier 추가
    
    * Feat: Clova API 클라이언트 구현
    
    * Refactor: OCR 요청 기본값을 상수로 변경
    
    * Test: Clova API 클라이언트 단위 테스트 작성
    
    * Refactor: API 클라이언트 패키지 구조 및 메서드명 수정
    
    - global.client
    -> global.client.clova
    -> global.client.openai
    
    - 프로퍼티도 client와 같은 패키지에서 관리하기 위함
    
    * Feat: GPT API 클라이언트 구현
    
    - OCR 결과값 파싱을 위한 gpt_instruction.txt 작성
    - 책임 분리를 위해 instruction.txt를 관리하는 GptPromptManager 구현
    - 타임아웃, 5xx 에러 시 요청 재시도
    -> GPT의 응답 시간 편차가 크기에 재시도 횟수가 Clova ocr보다 많음
    
    * Test: GPT API 클라이언트 단위 테스트 작성
    
    * Docs: GPT instruction 작성
    
    * Feat: 영수증 OCR 기능 구현
    
    * Feat: ReceiptController 구현
    
    - mockAnalyzeReceipt는 더미 Api
    
    * Refactor: 고정값 상수 분리 및 오타 수정
    
    * Fix: 임시적으로 properties 기본값 설정
    
    * Fix: annotationProcessor 추가
    
    * Fix: properties 기본값 제거
    
    * Remove: properties 파일 트래킹 목록에서 삭제
    
    * Style: 전체 코드 formatting
    
    * Docs: Gpt 프롬프트 수정
    
    * Feat: clova, gpt API 요청 객체 불변성 수정
    
    * Feat: ReceiptService 예외 처리 추가
    
    * Comment: Test 코드 주석 추가

---
+++ src/main/java/kappzzang/jeongsan/dto/Image.java
@@ -0,0 +1,5 @@
+package kappzzang.jeongsan.dto;
+
+public record Image(String format, String url, String data, String name) {
+
+}
---
+++ src/main/java/kappzzang/jeongsan/global/config/ApiClientConfig.java
@@ -0,0 +1,58 @@
+package kappzzang.jeongsan.global.config;
+
+import java.time.Duration;
+import kappzzang.jeongsan.global.client.clova.ClovaOcrProperties;
+import kappzzang.jeongsan.global.client.openai.OpenAiProperties;
+import kappzzang.jeongsan.global.interceptor.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.web.client.RestClient;
+
+@EnableRetry
+@Configuration
+public class ApiClientConfig {
+
+    private static final String CLOVA_OCR_SECRET_HEADER = "X-OCR-SECRET";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private final Long apiTimeout;
+    private final ClovaOcrProperties clovaOcrProperties;
+    private final OpenAiProperties openAiProperties;
+
+    public ApiClientConfig(@Value("${external.api.timeout}") Long apiTimeout,
+        ClovaOcrProperties clovaOcrProperties, OpenAiProperties openAiProperties) {
+        this.apiTimeout = apiTimeout;
+        this.clovaOcrProperties = clovaOcrProperties;
+        this.openAiProperties = openAiProperties;
+    }
+
+    @Bean
+    @Qualifier(value = "clovaOcrClientBuilder")
+    public RestClient.Builder clovaOcrClientBuilder() {
+        return getDefaultRestClientBuilder()
+            .defaultHeader(CLOVA_OCR_SECRET_HEADER, clovaOcrProperties.key());
+    }
+
+    @Bean
+    @Qualifier(value = "openAiClientBuilder")
+    public RestClient.Builder openAiClientBuilder() {
+        return getDefaultRestClientBuilder()
+            .defaultHeader(AUTHORIZATION_HEADER,
+                openAiProperties.authType() + openAiProperties.key());
+    }
+
+    private RestClient.Builder getDefaultRestClientBuilder() {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+            .withConnectTimeout(Duration.ofMillis(apiTimeout))
+            .withReadTimeout(Duration.ofMillis(apiTimeout));
+
+        return RestClient.builder()
+            .requestFactory(ClientHttpRequestFactories.get(settings))
+            .requestInterceptor(new LoggingInterceptor());
+    }
+}
