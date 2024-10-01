9a21b6f01f5c8fbb9b41bd1aebf10872abc2f8a9
minju26 <mjalswn26@gmail.com>
2024-10-02 00:21:28 +0900

    Style: 불필요한 개행 삭제

--- src/main/java/kappzzang/jeongsan/repository/MemberRepository.java
+++ src/main/java/kappzzang/jeongsan/repository/MemberRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findMemberById(Long id);
-
 }
