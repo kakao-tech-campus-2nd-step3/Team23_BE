8d23bdc1e8e87f267bec56f638b94b724fee7ff4
Lucerna00 <pjs3859811@naver.com>
2024-10-03 03:33:15 +0900

    Docs: 로그인 API description 추가

--- src/main/java/kappzzang/jeongsan/controller/docs/MemberControllerInterface.java
+++ src/main/java/kappzzang/jeongsan/controller/docs/MemberControllerInterface.java
@@ -5,11 +5,19 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kappzzang.jeongsan.dto.request.JoinTeamRequest;
+import kappzzang.jeongsan.dto.request.LoginRequest;
+import kappzzang.jeongsan.dto.response.LoginResponse;
 import kappzzang.jeongsan.global.common.JeongsanApiResponse;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "회원 관리", description = "JWT 반환, 멤버 그룹 참여, 초대 현황 등을 관리하는 API")
 public interface MemberControllerInterface {
+
+    @Operation(summary = "로그인 API", description = "카카오 토큰으로 회원가입 및 로그인하여 액세스 토큰을 받는 API")
+    @ApiResponses({@ApiResponse(responseCode = "201", description = "로그인 성공"),
+        @ApiResponse(responseCode = "408", description = "외부 API 요청 시간 초과. (ErrorCode-E408001"),
+        @ApiResponse(responseCode = "500", description = "외부 API 호출 중 오류 발생. (ErrorCode-E500003)")})
+    ResponseEntity<JeongsanApiResponse<LoginResponse>> login(LoginRequest loginRequest);
 
     @Operation(summary = "모임 초대 수락 API", description = "모임 초대 링크를 받은 사용자가 모임 참여를 수락하여 모임에 참여하도록 하는 API")
     @ApiResponses({@ApiResponse(responseCode = "204", description = "모임 초대 수락, 모임 참여 성공"),
