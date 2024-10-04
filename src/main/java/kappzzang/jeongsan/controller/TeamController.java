8fdca1bce615e99c0d3c31880ec398b32c7d1169
Wooseok Jang <us4c0d3@gmail.com>
2024-10-04 17:25:17 +0900

    Document/issue#46: 지출 내역 및 모임 관련 API에 대한 명세 작성 (#48)
    * Docs: 모임 관련 API 명세 추가
    
    * Refactor: 생성자 롬복으로 대체
    
    * Docs: 지출 내역 관련 API에 대한 명세 작성
    
    * Docs: Tag.name을 일관성 있게 수정
    
    * Docs: 모임 종료 API가 성공했을 때 응답 데이터가 없는 것을 명시적으로 표시

--- src/main/java/kappzzang/jeongsan/controller/TeamController.java
+++ src/main/java/kappzzang/jeongsan/controller/TeamController.java
@@ -2,11 +2,13 @@ package kappzzang.jeongsan.controller;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import java.util.List;
+import kappzzang.jeongsan.controller.docs.TeamControllerInterface;
 import kappzzang.jeongsan.dto.request.CloseTeamRequest;
 import kappzzang.jeongsan.dto.response.TeamResponse;
 import kappzzang.jeongsan.global.common.JeongsanApiResponse;
 import kappzzang.jeongsan.global.common.enumeration.SuccessType;
 import kappzzang.jeongsan.service.TeamService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,14 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/teams")
-public class TeamController {
+@RequiredArgsConstructor
+public class TeamController implements TeamControllerInterface {
 
     private final TeamService teamService;
 
-    public TeamController(TeamService teamService) {
-        this.teamService = teamService;
-    }
-
+    @Override
     @GetMapping
     public ResponseEntity<JeongsanApiResponse<List<TeamResponse>>> getTeams(
         @RequestParam("isClosed") Boolean isClosed) {
@@ -33,6 +33,7 @@ public class TeamController {
         return JeongsanApiResponse.success(SuccessType.TEAM_LIST_LOADED, data);
     }
 
+    @Override
     @PatchMapping("/{teamId}")
     public ResponseEntity<JeongsanApiResponse<Void>> closeTeam(
         @PathVariable("teamId") Long teamId, @RequestBody CloseTeamRequest request) {
---
+++ src/main/java/kappzzang/jeongsan/controller/docs/ExpenseControllerInterface.java
@@ -0,0 +1,33 @@
+package kappzzang.jeongsan.controller.docs;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kappzzang.jeongsan.dto.response.ExpenseResponse;
+import kappzzang.jeongsan.global.common.JeongsanApiResponse;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "지출 목록 관리", description = "지출 내역에 대한 조회, 저장 등 관리하는 API")
+public interface ExpenseControllerInterface {
+
+    @Operation(summary = "지출 내역 목록 조회 API", description = "지출 내역 목록을 쿼리 파라미터의 조건에 따라 조회하는 API")
+    @Parameters({
+        @Parameter(name = "teamId", description = "지출 내역 조회를 원하는 모임의 ID"),
+        @Parameter(name = "state", description = "지출 내역의 상태를 지정하는 쿼리 파라미터(`ongoing`, `pending`, `completed`)"),
+        @Parameter(name = "isChecked", description = "`정산 중`상태의 지출 목록 중 사용자의 현재 선택한 상태를 지정하는 쿼리 파라미터")
+    })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "지출 내역 목록을 성공적으로 조회",
+            content = @Content(schema = @Schema(implementation = ExpenseResponse.class))),
+        @ApiResponse(responseCode = "400", description = "누락된 쿼리 파라미터가 존재 (ErrorCode=E400)"),
+        @ApiResponse(responseCode = "400", description = "`state`에 잘못된 값 입력. `ongoing`, `pending`, `completed`만 가능 (ErrorCode-E400)"),
+        @ApiResponse(responseCode = "404", description = "`teamId`에 해당하는 모임이 존재하지 않음. (ErrorCode-E404)")
+    })
+    ResponseEntity<JeongsanApiResponse<ExpenseResponse>> getAllExpenses(Long teamId, String state,
+        Boolean isChecked);
+}
