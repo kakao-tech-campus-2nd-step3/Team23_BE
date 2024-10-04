78510175d5916a6a117ff76d30fb002922d6c357
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2024-10-04 11:48:48 +0900

    Feature/issue#41: 카카오 토큰을 이용한 로그인 (#44)
    * Feat: Kakao API 클라이언트 구현
    
    * Rename: KakaoProperties를 KakaoProfileProperties로 이름 변경
    
    * Test: Kakao API 클라이언트 테스트 추가
    
    * Chore: KakaoProfileResponse DTO를 global.client로 이동
    
    * Feat: JWT 생성 메서드 추가
    
    * Fix: Kakao API 클라이언트 빌더의 매개변수 제거
    
    * Feat: 로그인 구현
    
    * Test: 로그인 Service 단위 테스트 추가
    
    * Refactor: Kakao API 클라이언트 테스트 로직 변경
    
    - 비교를 위한 JSON 문자열 추가
    
    * Docs: 로그인 API description 추가
    
    * Refactor: Kakao API 클라이언트 예외 처리 제거
    
    - 이미 예외 처리 핸들러가 있기에 제거
    
    * Refactor: DTO 필드를 카멜 표기법으로 변경
    
    * Comment: 로그인 Service 주석 제거
    
    * Refactor: member의 KakaoToken 필드 복원
    
    * Style: 코드 정렬

--- src/main/java/kappzzang/jeongsan/global/util/JwtUtil.java
+++ src/main/java/kappzzang/jeongsan/global/util/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,9 +17,11 @@ public class JwtUtil {
     private static final String BEARER_PREFIX = "Bearer ";
 
     private final SecretKey secretKey;
+    private final long expirationTime;
 
-    public JwtUtil(@Value("${jwt.secret-key}") String secret) {
+    public JwtUtil(@Value("${jwt.secret-key}") String secret, @Value("${jwt.expiration-time}") long expirationTime) {
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secret));
+        this.expirationTime = expirationTime;
     }
 
     public String resolveToken(HttpServletRequest request) {
@@ -36,5 +39,14 @@ public class JwtUtil {
             .parseSignedClaims(token)
             .getPayload()
             .getSubject());
+    }
+
+    public String createToken(Long id) {
+        Date now = new Date();
+        return Jwts.builder()
+            .subject(Long.toString(id))
+            .expiration(new Date(now.getTime() + expirationTime))
+            .signWith(secretKey)
+            .compact();
     }
 }
--- src/main/java/kappzzang/jeongsan/service/MemberService.java
+++ src/main/java/kappzzang/jeongsan/service/MemberService.java
@@ -4,24 +4,48 @@ import static kappzzang.jeongsan.global.common.enumeration.ErrorType.NOT_INVITED
 import static kappzzang.jeongsan.global.common.enumeration.ErrorType.TEAM_NOT_FOUND;
 import static kappzzang.jeongsan.global.common.enumeration.ErrorType.USER_NOT_FOUND;
 
-import jakarta.transaction.Transactional;
 import kappzzang.jeongsan.domain.Member;
 import kappzzang.jeongsan.domain.Team;
 import kappzzang.jeongsan.domain.TeamMember;
+import kappzzang.jeongsan.dto.request.LoginRequest;
+import kappzzang.jeongsan.dto.response.LoginResponse;
+import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse;
+import kappzzang.jeongsan.global.client.kakao.KakaoApiClient;
 import kappzzang.jeongsan.global.exception.JeongsanException;
+import kappzzang.jeongsan.global.util.JwtUtil;
 import kappzzang.jeongsan.repository.MemberRepository;
 import kappzzang.jeongsan.repository.TeamMemberRepository;
 import kappzzang.jeongsan.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
-    private final TeamRepository teamRepository;
+    private static final String BEARER = "Bearer";
+
+    private final KakaoApiClient kakaoApiClient;
+    private final JwtUtil jwtUtil;
     private final MemberRepository memberRepository;
+    private final TeamRepository teamRepository;
     private final TeamMemberRepository teamMemberRepository;
+
+    @Transactional
+    public LoginResponse login(LoginRequest loginRequest) {
+        KakaoProfileResponse kakaoProfileResponse = kakaoApiClient.getKakaoProfile(loginRequest.accessToken());
+
+        Member member = memberRepository.findById(kakaoProfileResponse.id())
+            .orElseGet(kakaoProfileResponse::toMember);
+        String token = jwtUtil.createToken(kakaoProfileResponse.id());
+        member = member.toBuilder()
+            .token(token)
+            .build();
+        memberRepository.save(member);
+
+        return new LoginResponse(BEARER, token);
+    }
 
     @Transactional
     public void acceptInvite(Long teamId, Long memberId) {
