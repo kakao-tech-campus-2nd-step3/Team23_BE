21d1dfc9032318f1c9df366ac28b48b94814f4ed
Wooseok Jang <us4c0d3@gmail.com>
2024-10-11 19:59:30 +0900

    Feature/issue#67: 모임 생성 기능 추가 (#68)
    * Feat: 모임 생성 기능 추가
    
    * Feat: 모임 멤버 추가 기능 구현
    
    * Feat: Team.addMember 기능 추가에 따른 로직 수정
    
    * Docs: createTeam API 명세 작성
    
    * Feat: Team과 TeamMember간 영속성 전이를 위한 Cascade 설정 추가
    
    * Feat: 모임 이름에 공백 문자열을 허용하지 않게 `@NotBlank`로 validation 변경
    
    * Feat: 모임 생성 시 members가 null 또는 빈 리스트인 경우에 대한 예외 처리
    
    * Chore: validation 의존성 추가

--- src/main/java/kappzzang/jeongsan/repository/TeamRepository.java
+++ src/main/java/kappzzang/jeongsan/repository/TeamRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import kappzzang.jeongsan.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
@@ -12,4 +13,10 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
         "JOIN FETCH tm.member " +
         "WHERE t.isClosed = :isClosed")
     List<Team> findByIsClosed(Boolean isClosed);
+
+    @Query("SELECT CASE WHEN COUNT(t) > 0 THEN TRUE ELSE FALSE END "
+        + "FROM Team t "
+        + "JOIN TeamMember tm ON t.id = tm.team.id "
+        + "WHERE t.name = :name AND tm.member.id = :memberId")
+    Boolean existsByNameAndMemberId(@Param("name") String name, @Param("memberId") Long memberId);
 }
