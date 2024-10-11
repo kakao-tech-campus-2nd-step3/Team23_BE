beea062b0cf6b7f7308e06bc785034374adf6c18
us4c0d3 <jwseok312@gmail.com>
2024-10-11 17:41:39 +0900

    Feat: 모임 이름에 공백 문자열을 허용하지 않게 `@NotBlank`로 validation 변경

--- src/main/java/kappzzang/jeongsan/dto/request/CreateTeamRequest.java
+++ src/main/java/kappzzang/jeongsan/dto/request/CreateTeamRequest.java
@@ -1,11 +1,12 @@
 package kappzzang.jeongsan.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record CreateTeamRequest(
-    @NotNull
+    @NotBlank
     @Size(min = 1, max = 15)
     String name,
     @NotNull
