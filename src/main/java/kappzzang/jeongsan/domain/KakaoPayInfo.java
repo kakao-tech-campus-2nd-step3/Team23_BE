47b7fdb15713d9b98e12f152f417d5b3cd3094cb
minju26 <110724254+minju26@users.noreply.github.com>
2024-10-15 14:54:34 +0900

    Refactor/issue#73: 엔티티 클래스 수정 (#76)
    * Refactor: KakaoPayInfo 임베디드 타입으로 변경
    
    * Refactor: `nullable = false` 설정
    
    * Chore: repository test 주석처리
    
    - kakaoPayInfo 테이블 없음 에러
    - NOT NOLL 필드 에러
    - 리팩토링 후 수정 예정

--- src/main/java/kappzzang/jeongsan/domain/KakaoPayInfo.java
+++ src/main/java/kappzzang/jeongsan/domain/KakaoPayInfo.java
@@ -1,28 +1,15 @@
 package kappzzang.jeongsan.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
+@Embeddable
 @NoArgsConstructor
 public class KakaoPayInfo {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @OneToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
-
     private String payUrl;
-    private String accessToken;
-    private String refreshToken;
+    private String payAccessToken;
+    private String payRefreshToken;
 }
