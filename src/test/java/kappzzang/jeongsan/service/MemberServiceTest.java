092910af694f61ab125523367fbc242f5c259476
Lucerna00 <pjs3859811@naver.com>
2024-10-17 15:14:32 +0900

    Test: access 토큰 재발급 테스트 추가

--- src/test/java/kappzzang/jeongsan/service/MemberServiceTest.java
+++ src/test/java/kappzzang/jeongsan/service/MemberServiceTest.java
@@ -1,20 +1,28 @@
 package kappzzang.jeongsan.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 
+import io.jsonwebtoken.Claims;
 import java.util.Optional;
 import kappzzang.jeongsan.domain.Member;
 import kappzzang.jeongsan.dto.request.LoginRequest;
+import kappzzang.jeongsan.dto.request.RefreshRequest;
 import kappzzang.jeongsan.dto.response.LoginResponse;
+import kappzzang.jeongsan.dto.response.RefreshResponse;
 import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse;
 import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse.ForPartner;
 import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse.KakaoAccount;
 import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse.KakaoAccount.Profile;
 import kappzzang.jeongsan.global.client.kakao.KakaoApiClient;
+import kappzzang.jeongsan.global.exception.JeongsanException;
 import kappzzang.jeongsan.global.util.JwtUtil;
 import kappzzang.jeongsan.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -46,7 +54,7 @@ public class MemberServiceTest {
     @Test
     @DisplayName("카카오 로그인으로 회원가입 및 토큰 발급 성공")
     void login() {
-        // Given
+        // given
         KakaoProfileResponse kakaoProfileResponse = createKakaoProfileResponse();
         given(kakaoApiClient.getKakaoProfile(anyString())).willReturn(kakaoProfileResponse);
         given(memberRepository.findByKakaoId(anyString())).willReturn(Optional.empty());
@@ -54,14 +62,56 @@ public class MemberServiceTest {
         given(jwtUtil.createAccessToken(any())).willReturn(TEST_ACCESS_TOKEN);
         given(jwtUtil.createRefreshToken()).willReturn(TEST_REFRESH_TOKEN);
 
-        // When
+        // when
         LoginResponse loginResponse = memberService.login(new LoginRequest(anyString()));
 
-        // Then
+        // then
         then(memberRepository).should(times(2)).save(any(Member.class));
-        then(loginResponse.tokenType()).equals(BEARER);
-        then(loginResponse.accessToken()).equals(TEST_ACCESS_TOKEN);
-        then(loginResponse.refreshToken()).equals(TEST_REFRESH_TOKEN);
+        assertThat(loginResponse.tokenType()).isEqualTo(BEARER);
+        assertThat(loginResponse.accessToken()).isEqualTo(TEST_ACCESS_TOKEN);
+        assertThat(loginResponse.refreshToken()).isEqualTo(TEST_REFRESH_TOKEN);
+    }
+
+    @Test
+    @DisplayName("액세스 토큰 재발급 실패 - 일치하지 않은 리프레시 토큰")
+    void refreshWithMismatchedRefreshToken() {
+        // given
+        RefreshRequest refreshRequest = new RefreshRequest("RefreshToken");
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(createMember()));
+
+        // when & then
+        assertThatThrownBy(() -> memberService.refresh(anyLong(), refreshRequest))
+            .isInstanceOf(JeongsanException.class);
+    }
+
+    @Test
+    @DisplayName("액세스 토큰 재발급 실패 - 유효하지 않은 리프레시 토큰")
+    void refreshWithInvalidRefreshToken() {
+        // given
+        RefreshRequest refreshRequest = new RefreshRequest(TEST_REFRESH_TOKEN);
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(createMember()));
+        given(jwtUtil.parseClaims(TEST_REFRESH_TOKEN)).willThrow(JeongsanException.class);
+
+        // when & then
+        assertThatThrownBy(() -> memberService.refresh(anyLong(), refreshRequest))
+            .isInstanceOf(JeongsanException.class);
+    }
+
+    @Test
+    @DisplayName("액세스 토큰 재발급 성공")
+    void refresh() {
+        // given
+        RefreshRequest refreshRequest = new RefreshRequest(TEST_REFRESH_TOKEN);
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(createMember()));
+        given(jwtUtil.parseClaims(TEST_REFRESH_TOKEN)).willReturn(mock(Claims.class));
+        given(jwtUtil.createAccessToken(anyLong())).willReturn(TEST_ACCESS_TOKEN);
+
+        // when
+        RefreshResponse refreshResponse = memberService.refresh(anyLong(), refreshRequest);
+
+        // then
+        assertThat(refreshResponse.tokenType()).isEqualTo(BEARER);
+        assertThat(refreshResponse.accessToken()).isEqualTo(TEST_ACCESS_TOKEN);
     }
 
     private KakaoProfileResponse createKakaoProfileResponse() {
@@ -69,5 +119,11 @@ public class MemberServiceTest {
         KakaoAccount kakaoAccount = new KakaoAccount("sample@sample.com", profile);
         ForPartner forPartner = new ForPartner("550e8400-e29b-41d4-a716-446655440000");
         return new KakaoProfileResponse(kakaoAccount, forPartner);
+    }
+
+    private Member createMember() {
+        return Member.builder()
+            .refreshToken(TEST_REFRESH_TOKEN)
+            .build();
     }
 }
