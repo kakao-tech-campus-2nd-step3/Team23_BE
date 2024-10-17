02246490946aae49f6e6f83b0c01e139ba4317a4
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2024-10-17 20:49:41 +0900

    Feature/issue#80: Access 토큰 재발급 로직 추가 (#83)
    * Fix: Date 대신 LocalDateTime 사용
    
    - Date는 불변성 객체가 아님
    - Date는 시간대 정보를 포함하지 않음
    
    * Refactor: JWT 검증 메서드 추가
    
    * Feat: access 토큰 재발급 로직 추가
    
    * Docs: 회원 API 명세에 파라미터 설명 추가
    
    * Fix: 로그인 request 필드에 validation 추가
    
    * Test: access 토큰 재발급 테스트 추가
    
    * Refactor: refresh 토큰 검증 메서드 분리
    
    * Fix: 누락된 RequestBody 어노테이션 추가
    
    * Fix: 시간대를 Asia/Seoul로 변경

--- src/main/java/kappzzang/jeongsan/controller/MemberController.java
+++ src/main/java/kappzzang/jeongsan/controller/MemberController.java
@@ -2,15 +2,19 @@ package kappzzang.jeongsan.controller;
 
 import static kappzzang.jeongsan.global.common.enumeration.SuccessType.JOIN_SUCCESS;
 
+import jakarta.validation.Valid;
 import kappzzang.jeongsan.controller.docs.MemberControllerInterface;
 import kappzzang.jeongsan.dto.request.JoinTeamRequest;
 import kappzzang.jeongsan.dto.request.LoginRequest;
+import kappzzang.jeongsan.dto.request.RefreshRequest;
 import kappzzang.jeongsan.dto.response.LoginResponse;
+import kappzzang.jeongsan.dto.response.RefreshResponse;
 import kappzzang.jeongsan.global.common.JeongsanApiResponse;
 import kappzzang.jeongsan.global.common.enumeration.SuccessType;
 import kappzzang.jeongsan.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,9 +31,17 @@ public class MemberController implements MemberControllerInterface {
     @Override
     @PostMapping("/token")
     public ResponseEntity<JeongsanApiResponse<LoginResponse>> login(
-        @RequestBody LoginRequest loginRequest) {
+        @Valid @RequestBody LoginRequest loginRequest) {
         return JeongsanApiResponse.success(SuccessType.LOGGED_IN,
             memberService.login(loginRequest));
+    }
+
+    @Override
+    @PostMapping("/token/refresh")
+    public ResponseEntity<JeongsanApiResponse<RefreshResponse>> refresh(
+        @AuthenticationPrincipal Long memberId, @Valid @RequestBody RefreshRequest refreshRequest) {
+        return JeongsanApiResponse.success(SuccessType.ACCESS_TOKEN_REISSUED,
+            memberService.refresh(memberId, refreshRequest));
     }
 
     @Override
--- src/main/java/kappzzang/jeongsan/controller/docs/MemberControllerInterface.java
+++ src/main/java/kappzzang/jeongsan/controller/docs/MemberControllerInterface.java
@@ -1,12 +1,16 @@
 package kappzzang.jeongsan.controller.docs;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kappzzang.jeongsan.dto.request.JoinTeamRequest;
 import kappzzang.jeongsan.dto.request.LoginRequest;
+import kappzzang.jeongsan.dto.request.RefreshRequest;
 import kappzzang.jeongsan.dto.response.LoginResponse;
+import kappzzang.jeongsan.dto.response.RefreshResponse;
 import kappzzang.jeongsan.global.common.JeongsanApiResponse;
 import org.springframework.http.ResponseEntity;
 
@@ -14,16 +18,30 @@ import org.springframework.http.ResponseEntity;
 public interface MemberControllerInterface {
 
     @Operation(summary = "로그인 API", description = "카카오 토큰으로 회원가입 및 로그인하여 액세스 토큰을 받는 API")
+    @Parameters({
+        @Parameter(name = "tokenType", description = "카카오 액세스 토큰 타입"),
+        @Parameter(name = "accessToken", description = "카카오 액세스 토큰")
+    })
     @ApiResponses({@ApiResponse(responseCode = "201", description = "로그인 성공"),
         @ApiResponse(responseCode = "408", description = "외부 API 요청 시간 초과. (ErrorCode-E408001"),
         @ApiResponse(responseCode = "500", description = "외부 API 호출 중 오류 발생. (ErrorCode-E500003)")})
     ResponseEntity<JeongsanApiResponse<LoginResponse>> login(LoginRequest loginRequest);
 
+    @Operation(summary = "액세스 토큰 재발급 API", description = "리프레시 토큰으로 액세스 토큰을 받는 API")
+    @Parameters({
+        @Parameter(name = "refreshToken", description = "서비스 서버 리프레시 토큰")
+    })
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "액세스 토큰 재발급 성공"),
+        @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음. (ErrorCode-E404001)"),
+        @ApiResponse(responseCode = "403", description = "리프레시 토큰이 유효하지 않음. (ErrorCode-E403001)")})
+    ResponseEntity<JeongsanApiResponse<RefreshResponse>> refresh(Long memberId,
+        RefreshRequest refreshRequest);
+
     @Operation(summary = "모임 초대 수락 API", description = "모임 초대 링크를 받은 사용자가 모임 참여를 수락하여 모임에 참여하도록 하는 API")
     @ApiResponses({@ApiResponse(responseCode = "204", description = "모임 초대 수락, 모임 참여 성공"),
         @ApiResponse(responseCode = "400", description = "모임에 초대 되지 않은 멤버. (ErrorCode-E400002"),
         @ApiResponse(responseCode = "400", description = "이미 모임에 참여중인 멤버. (ErrorCode-E400003)"),
-        @ApiResponse(responseCode = "404", description = "잘못된 teamId, 모임을 찾을 수 없음. (ErrorCode-E404002)"),
-        @ApiResponse(responseCode = "404", description = "잘못된 memberId, 사용자를 찾을 수 없음. (ErrorCode-E404003)")})
+        @ApiResponse(responseCode = "404", description = "잘못된 memberId, 사용자를 찾을 수 없음. (ErrorCode-E404001)"),
+        @ApiResponse(responseCode = "404", description = "잘못된 teamId, 모임을 찾을 수 없음. (ErrorCode-E404002)")})
     ResponseEntity<JeongsanApiResponse<Void>> joinTeam(Long teamId, JoinTeamRequest request);
 }
