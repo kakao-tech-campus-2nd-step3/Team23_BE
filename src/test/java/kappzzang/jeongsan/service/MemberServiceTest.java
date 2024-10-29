221b41e9f23febfa3c9fdc5e6fe0eb39d4e68314
Lucerna00 <pjs3859811@naver.com>
2024-10-29 14:45:20 +0900

    Refactor: 변경된 로그인 로직에 맞춰 테스트 수정

--- src/test/java/kappzzang/jeongsan/service/MemberServiceTest.java
+++ src/test/java/kappzzang/jeongsan/service/MemberServiceTest.java
@@ -7,7 +7,6 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
 
 import java.util.Optional;
 import kappzzang.jeongsan.domain.Member;
@@ -15,11 +14,6 @@ import kappzzang.jeongsan.dto.request.LoginRequest;
 import kappzzang.jeongsan.dto.request.RefreshRequest;
 import kappzzang.jeongsan.dto.response.LoginResponse;
 import kappzzang.jeongsan.dto.response.RefreshResponse;
-import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse;
-import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse.ForPartner;
-import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse.KakaoAccount;
-import kappzzang.jeongsan.global.client.dto.response.KakaoProfileResponse.KakaoAccount.Profile;
-import kappzzang.jeongsan.global.client.kakao.KakaoApiClient;
 import kappzzang.jeongsan.global.common.enumeration.ErrorType;
 import kappzzang.jeongsan.global.exception.JeongsanException;
 import kappzzang.jeongsan.global.util.JwtUtil;
@@ -39,9 +33,6 @@ public class MemberServiceTest {
     private static final String TEST_REFRESH_TOKEN = "TestRefreshToken";
 
     @Mock
-    private KakaoApiClient kakaoApiClient;
-
-    @Mock
     private JwtUtil jwtUtil;
 
     @Mock
@@ -51,13 +42,22 @@ public class MemberServiceTest {
     private MemberService memberService;
 
     @Test
-    @DisplayName("카카오 로그인으로 회원가입 및 토큰 발급 성공")
+    @DisplayName("로그인 실패 - 회원가입 필요")
+    void loginWithoutRegistration() {
+        // given
+        given(memberRepository.findByEmail(anyString())).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> memberService.login(new LoginRequest(anyString())))
+            .isInstanceOf(JeongsanException.class)
+            .hasFieldOrPropertyWithValue("errorType", ErrorType.USER_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
     void login() {
         // given
-        KakaoProfileResponse kakaoProfileResponse = createKakaoProfileResponse();
-        given(kakaoApiClient.getKakaoProfile(anyString())).willReturn(kakaoProfileResponse);
-        given(memberRepository.findByKakaoId(anyString())).willReturn(Optional.empty());
-        given(memberRepository.save(any(Member.class))).willReturn(kakaoProfileResponse.toMember());
+        given(memberRepository.findByEmail(anyString())).willReturn(Optional.of(createMember()));
         given(jwtUtil.createAccessToken(any())).willReturn(TEST_ACCESS_TOKEN);
         given(jwtUtil.createRefreshToken()).willReturn(TEST_REFRESH_TOKEN);
 
@@ -65,10 +65,10 @@ public class MemberServiceTest {
         LoginResponse loginResponse = memberService.login(new LoginRequest(anyString()));
 
         // then
-        then(memberRepository).should(times(2)).save(any(Member.class));
-        assertThat(loginResponse.tokenType()).isEqualTo(BEARER);
-        assertThat(loginResponse.accessToken()).isEqualTo(TEST_ACCESS_TOKEN);
-        assertThat(loginResponse.refreshToken()).isEqualTo(TEST_REFRESH_TOKEN);
+        then(memberRepository).should().save(any(Member.class));
+        then(loginResponse.tokenType()).equals(BEARER);
+        then(loginResponse.accessToken()).equals(TEST_ACCESS_TOKEN);
+        then(loginResponse.refreshToken()).equals(TEST_REFRESH_TOKEN);
     }
 
     @Test
@@ -113,13 +113,6 @@ public class MemberServiceTest {
         // then
         assertThat(refreshResponse.tokenType()).isEqualTo(BEARER);
         assertThat(refreshResponse.accessToken()).isEqualTo(TEST_ACCESS_TOKEN);
-    }
-
-    private KakaoProfileResponse createKakaoProfileResponse() {
-        Profile profile = new Profile("홍길동", "http://yyy.kakao.com/.../img_110x110.jpg");
-        KakaoAccount kakaoAccount = new KakaoAccount("sample@sample.com", profile);
-        ForPartner forPartner = new ForPartner("550e8400-e29b-41d4-a716-446655440000");
-        return new KakaoProfileResponse(kakaoAccount, forPartner);
     }
 
     private Member createMember() {
