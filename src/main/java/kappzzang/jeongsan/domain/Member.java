c69fb4b02fc3cf3905a8a3aeda6dde885d76cc4b
minju26 <110724254+minju26@users.noreply.github.com>
2024-11-01 15:24:05 +0900

    Feat: 카카오 페이 송금 링크 조회 API 구현 (#101)

--- src/main/java/kappzzang/jeongsan/domain/Member.java
+++ src/main/java/kappzzang/jeongsan/domain/Member.java
@@ -50,4 +50,9 @@ public class Member extends BaseEntity {
         this.refreshToken = refreshToken;
         this.kakaoPayInfo = kakaoPayInfo;
     }
+
+    public Member(String nickname, KakaoPayInfo kakaoPayInfo) {
+        this.nickname = nickname;
+        this.kakaoPayInfo = kakaoPayInfo;
+    }
 }
