0d10e236527c425b79eb731f5ec607b0a8bf2673
minju26 <mjalswn26@gmail.com>
2024-11-01 16:52:20 +0900

    Fix: ItemInfo의 itemId값 검증 수정

--- src/main/java/kappzzang/jeongsan/dto/request/SavePersonalExpenseRequest.java
+++ src/main/java/kappzzang/jeongsan/dto/request/SavePersonalExpenseRequest.java
@@ -1,12 +1,13 @@
 package kappzzang.jeongsan.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
 
 public record SavePersonalExpenseRequest(@NotEmpty List<ItemInfo> items) {
 
-    public record ItemInfo(@NotEmpty Long itemId, @Positive Integer quantity) {
+    public record ItemInfo(@NotNull Long itemId, @Positive Integer quantity) {
 
     }
 }
