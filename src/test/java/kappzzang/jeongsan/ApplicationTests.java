7add815100767e39381f57f71dfad9322a19f5aa
us4c0d3 <jwseok312@gmail.com>
2024-11-05 13:26:07 +0900

    Chore: 테스트 프로퍼티 파일명 변경

--- src/test/java/kappzzang/jeongsan/ApplicationTests.java
+++ src/test/java/kappzzang/jeongsan/ApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:test.properties")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ApplicationTests {
 
     @Test
