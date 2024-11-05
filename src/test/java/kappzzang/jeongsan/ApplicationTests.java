5fd809404d0fff6e506eb0b1ad145bfeab5998b3
us4c0d3 <jwseok312@gmail.com>
2024-11-05 13:00:24 +0900

    Test: 테스트용 프로퍼티 추가

--- src/test/java/kappzzang/jeongsan/ApplicationTests.java
+++ src/test/java/kappzzang/jeongsan/ApplicationTests.java
@@ -2,8 +2,10 @@ package kappzzang.jeongsan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 class ApplicationTests {
 
     @Test
