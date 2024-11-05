90475aa51398628766ed0f36757d95507a2b3988
us4c0d3 <jwseok312@gmail.com>
2024-11-05 13:26:07 +0900

    Chore: 테스트 프로퍼티 파일명 변경

--- src/test/java/kappzzang/jeongsan/service/PersonalExpenseServiceTest.java
+++ src/test/java/kappzzang/jeongsan/service/PersonalExpenseServiceTest.java
@@ -36,7 +36,7 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestPropertySource(locations = "classpath:test.properties")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class PersonalExpenseServiceTest {
 
     @Autowired
