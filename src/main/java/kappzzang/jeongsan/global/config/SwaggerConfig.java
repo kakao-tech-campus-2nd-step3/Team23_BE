88f77c6f786754b3abe893ed81b2f64a00c132a6
minju26 <110724254+minju26@users.noreply.github.com>
2024-11-06 17:06:47 +0900

    Chore/issue#120: Swagger에 배포 주소 추가, CI 스크립트 수정 (#121)

--- src/main/java/kappzzang/jeongsan/global/config/SwaggerConfig.java
+++ src/main/java/kappzzang/jeongsan/global/config/SwaggerConfig.java
@@ -7,23 +7,29 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${url.local}")
+    private String localUrl;
+
+    @Value("${url.deploy}")
+    private String deployUrl;
+
+    @Value("${url.test}")
+    private String testUrl;
+
+
     @Bean
     public OpenAPI openAPI() {
 
         String accessToken = "accessToken";
-        String localUrl = "http://localhost:8080";
-
-        Server local = new Server();
-        local.setUrl(localUrl);
 
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(accessToken);
-
         Components components = new Components().addSecuritySchemes(accessToken,
             new SecurityScheme()
                 .name(accessToken)
@@ -35,7 +41,7 @@ public class SwaggerConfig {
         return new OpenAPI()
             .components(components)
             .info(apiInfo())
-            .servers(List.of(local))
+            .servers(serverList())
             .addSecurityItem(securityRequirement);
     }
 
@@ -44,5 +50,22 @@ public class SwaggerConfig {
             .title("KakaoTechCampus 정산(JeongSan) 서비스의 API 명세입니다.")
             .description("KakaoTechCampus Step3 Team23(KAppZzang) API Docs")
             .version("1.0.0");
+    }
+
+    private List<Server> serverList() {
+        Server local = new Server();
+        Server deploy = new Server();
+        Server test = new Server();
+
+        local.setUrl(localUrl);
+        local.setDescription("백엔드 로컬");
+
+        deploy.setUrl(deployUrl);
+        deploy.setDescription("Master 브랜치 배포");
+
+        test.setUrl(testUrl);
+        test.setDescription("Weekly 브랜치 배포");
+
+        return List.of(local, deploy, test);
     }
 }
