2b6a806a2409209bdafb6f16b727c6e99d72258a
dkswoals <a4445656@gmail.com>
2024-11-06 14:44:28 +0900

    Refactor: 사용되지 않는 어노테이션 제거

--- src/main/java/kappzzang/jeongsan/dto/response/SaveExpenseResponse.java
+++ src/main/java/kappzzang/jeongsan/dto/response/SaveExpenseResponse.java
@@ -1,9 +1,5 @@
 package kappzzang.jeongsan.dto.response;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record SaveExpenseResponse(Long expenseId) {
 
 }
