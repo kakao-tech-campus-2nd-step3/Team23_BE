a753dde866bb66f0e5f721c002678f9023968923
us4c0d3 <us4c0d3@gmail.com>
2024-11-06 16:33:22 +0900

    Feat: teamId를 통한 모임 조회 기능 구현

--- src/main/java/kappzzang/jeongsan/controller/TeamController.java
+++ src/main/java/kappzzang/jeongsan/controller/TeamController.java
@@ -40,6 +40,12 @@ public class TeamController implements TeamControllerInterface {
         return JeongsanApiResponse.success(SuccessType.TEAM_LIST_LOADED, data);
     }
 
+    @GetMapping("{teamId}")
+    public ResponseEntity<JeongsanApiResponse<TeamResponse>> getTeam(@PathVariable Long teamId) {
+        TeamResponse data = teamService.getTeam(teamId);
+        return JeongsanApiResponse.success(SuccessType.TEAM_LIST_LOADED, data);
+    }
+
     @Override
     @GetMapping("{teamId}")
     public ResponseEntity<JeongsanApiResponse<TeamResponse>> getTeam(@PathVariable Long teamId) {
