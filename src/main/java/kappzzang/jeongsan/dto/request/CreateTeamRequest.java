b74e1a5b3fd7c6c6c5826eebf285df34ba451109
minju26 <mjalswn26@gmail.com>
2024-11-06 23:04:24 +0900

    Refactor: 멤버 정보 리스트 validation 수정

--- src/main/java/kappzzang/jeongsan/dto/request/CreateTeamRequest.java
+++ src/main/java/kappzzang/jeongsan/dto/request/CreateTeamRequest.java
@@ -1,7 +1,6 @@
 package kappzzang.jeongsan.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -12,7 +11,7 @@ public record CreateTeamRequest(
     String name,
     @NotNull
     String subject,
-    @NotEmpty
+    @NotNull
     List<String> members
 ) {
 
