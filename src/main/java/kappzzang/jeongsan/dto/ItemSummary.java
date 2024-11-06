edc913f523bfb245ddd4ef4ee4ebd39b2b712b71
An Jaemin <a4445656@gmail.com>
2024-11-06 23:32:55 +0900

    Refactor: 사용되지 않는 어노테이션, 생성자 제거 및 기타 리펙토링 (#126)
    * Refactor: 사용되지 않는 생성자 제거
    
    * Refactor: 사용되지 않는 어노테이션 제거
    
    * Refactor: 외부API 클라이언트 Qualifier 명시 추가

--- src/main/java/kappzzang/jeongsan/dto/ItemSummary.java
+++ src/main/java/kappzzang/jeongsan/dto/ItemSummary.java
@@ -1,7 +1,5 @@
 package kappzzang.jeongsan.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,7 +7,6 @@ import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import kappzzang.jeongsan.domain.Item;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record ItemSummary(@NotBlank @Size(max = 20) String name,
                           @NotNull @PositiveOrZero @Max(value = 1000000) Integer quantity,
                           @NotNull @PositiveOrZero Integer unitPrice) {
