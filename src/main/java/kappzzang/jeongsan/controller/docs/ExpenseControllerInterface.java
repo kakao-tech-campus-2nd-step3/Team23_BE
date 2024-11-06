ef126888c384b667612e63d9081bb9a258e03615
Lucerna00 <pjs3859811@naver.com>
2024-11-07 01:46:41 +0900

    Refactor: 커스텀 어노테이션 사용

--- src/main/java/kappzzang/jeongsan/controller/docs/ExpenseControllerInterface.java
+++ src/main/java/kappzzang/jeongsan/controller/docs/ExpenseControllerInterface.java
@@ -6,12 +6,13 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kappzzang.jeongsan.dto.request.CompleteExpensesRequest;
 import kappzzang.jeongsan.dto.request.SavePersonalExpenseRequest;
 import kappzzang.jeongsan.dto.response.ExpenseResponse;
+import kappzzang.jeongsan.global.common.ApiErrorTypeExample;
 import kappzzang.jeongsan.global.common.JeongsanApiResponse;
+import kappzzang.jeongsan.global.common.enumeration.ErrorType;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "지출 목록 관리", description = "지출 내역에 대한 조회, 저장 등 관리하는 API")
@@ -23,13 +24,9 @@ public interface ExpenseControllerInterface {
         @Parameter(name = "state", description = "지출 내역의 상태를 지정하는 쿼리 파라미터(`ongoing`, `pending`, `completed`)"),
         @Parameter(name = "isChecked", description = "`정산 중`상태의 지출 목록 중 사용자의 현재 선택한 상태를 지정하는 쿼리 파라미터")
     })
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "지출 내역 목록을 성공적으로 조회",
-            content = @Content(schema = @Schema(implementation = ExpenseResponse.class))),
-        @ApiResponse(responseCode = "400", description = "누락된 쿼리 파라미터가 존재 (ErrorCode=E400002)"),
-        @ApiResponse(responseCode = "400", description = "`state`에 잘못된 값 입력. `ongoing`, `pending`, `completed`만 가능 (ErrorCode-E400003)"),
-        @ApiResponse(responseCode = "404", description = "`teamId`에 해당하는 모임이 존재하지 않음. (ErrorCode-E404002)")
-    })
+    @ApiResponse(responseCode = "200", description = "지출 내역 목록을 성공적으로 조회", content = @Content(schema = @Schema(implementation = ExpenseResponse.class)))
+    @ApiErrorTypeExample({ErrorType.EXPENSE_MISSING_PARAM, ErrorType.EXPENSE_INVALID_STATE,
+        ErrorType.TEAM_NOT_FOUND})
     ResponseEntity<JeongsanApiResponse<ExpenseResponse>> getAllExpenses(Long memberId, Long teamId,
         String state, Boolean isChecked);
 
@@ -37,15 +34,10 @@ public interface ExpenseControllerInterface {
     @Parameters({
         @Parameter(name = "teamId", description = "상태 변경될 지출들의 모임 ID"),
     })
-    @ApiResponses({
-        @ApiResponse(responseCode = "204", description = "지출 상태 변경을 성공"),
-        @ApiResponse(responseCode = "400", description =
-            "이미 정산 완료된 지출 존재 (ErrorCode=E400007), "
-                + "아직 진행중인 상태의 지출 존재 (ErrorCode=E400008), "
-                + "요청 목록에 존재하지 않는 지출 포함 (ErrorCode=E400009), "
-                + "요청 목록에 타 모임의 지출이 포함(ErrorCode=E400010), "
-                + "요청 목록에 본인이 결제하지 않은 지출이 포함(ErrorCode=E400011)"),
-    })
+    @ApiResponse(responseCode = "204", description = "지출 상태 변경을 성공")
+    @ApiErrorTypeExample({ErrorType.EXPENSE_ALREADY_COMPLETED, ErrorType.EXPENSE_ONGOING,
+        ErrorType.EXPENSE_NOT_FOUND_ID, ErrorType.EXPENSE_INVALID_TEAM,
+        ErrorType.EXPENSE_INVALID_PAYER})
     ResponseEntity<JeongsanApiResponse<Void>> changeExpensesStatusComplete(
         CompleteExpensesRequest request, Long teamId, Long memberId);
 
@@ -54,17 +46,10 @@ public interface ExpenseControllerInterface {
         @Parameter(name = "teamId", description = "요청 멤버가 속한 모임의 ID"),
         @Parameter(name = "expenseId", description = "선택한 아이템이 속한 지출의 ID"),
     })
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "개인 소비 내역 저장 성공", content = @Content),
-        @ApiResponse(responseCode = "400", description =
-            "이전에 선택하여 저장 한 아이템이 포함된 요청 (ErrorCode-E400013), "
-                + "아이템 quantity 보다 많은 선택 수량 (ErrorCode=E400012)", content = @Content),
-        @ApiResponse(responseCode = "404", description =
-            "`teamId` 에 해당하는 데이터가 존재하지 않음 (ErrorCode-E404002), "
-                + "`expenseId` 에 해당하는 데이터가 존재하지 않음 (ErrorCode-E404004), "
-                + "`itemId` 에 해당하는 데이터가 존재하지 않음 (ErrorCode-E404007), "
-                + " 요청 멤버가 팀의 멤버가 아님 (ErrorCode-E404008), ", content = @Content)
-    })
+    @ApiResponse(responseCode = "200", description = "개인 소비 내역 저장 성공", content = @Content)
+    @ApiErrorTypeExample({ErrorType.INVALID_QUANTITY, ErrorType.ALREADY_CHECKED_ITEM,
+        ErrorType.TEAM_NOT_FOUND, ErrorType.EXPENSE_NOT_FOUND, ErrorType.ITEM_NOT_FOUND,
+        ErrorType.TEAM_MEMBER_NOT_FOUND})
     ResponseEntity<JeongsanApiResponse<Void>> savePersonalExpense(Long teamId,
         Long expenseId, Long memberId, SavePersonalExpenseRequest personalExpense);
 
