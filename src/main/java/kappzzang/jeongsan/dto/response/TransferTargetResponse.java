f316ecb8e282b931f4b75ab499a414c330d3eee7
minju26 <mjalswn26@gmail.com>
2024-11-06 23:46:30 +0900

    Refactor: member의 kakaoId 추가

--- src/main/java/kappzzang/jeongsan/dto/response/TransferTargetResponse.java
+++ src/main/java/kappzzang/jeongsan/dto/response/TransferTargetResponse.java
@@ -2,10 +2,10 @@ package kappzzang.jeongsan.dto.response;
 
 import kappzzang.jeongsan.domain.Member;
 
-public record TransferTargetResponse(Long memberId, String name, Integer amountDue,
+public record TransferTargetResponse(String kakaoId, Long memberId, String name, Integer amountDue,
                                      String profileImage) {
 
     public TransferTargetResponse(Member member, Integer totalPrice) {
-        this(member.getId(), member.getNickname(), totalPrice, member.getProfileImage());
+        this(member.getKakaoId(), member.getId(), member.getNickname(), totalPrice, member.getProfileImage());
     }
 }
