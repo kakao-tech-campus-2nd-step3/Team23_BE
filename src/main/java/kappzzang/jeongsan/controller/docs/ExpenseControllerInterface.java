2a6e200b38c9e5ea75f8d7ed5b0913c31d828e40
An Jaemin <a4445656@gmail.com>
2024-11-06 23:33:22 +0900

    Feature/issue#112: 카테고리 목록 조회 기능 구현 (#127)
    * Feat: 카테고리 조회 API 구현
    
    * Test: 카테고리 Service Test 작성
    
    * Docs: 카테고리 조회 API 명세 작성

--- src/main/java/kappzzang/jeongsan/controller/docs/ExpenseControllerInterface.java
+++ src/main/java/kappzzang/jeongsan/controller/docs/ExpenseControllerInterface.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kappzzang.jeongsan.dto.request.CompleteExpensesRequest;
 import kappzzang.jeongsan.dto.request.SavePersonalExpenseRequest;
+import kappzzang.jeongsan.dto.response.CategoryListResponse;
 import kappzzang.jeongsan.dto.response.ExpenseResponse;
 import kappzzang.jeongsan.global.common.JeongsanApiResponse;
 import org.springframework.http.ResponseEntity;
@@ -76,4 +77,11 @@ public interface ExpenseControllerInterface {
     })
     ResponseEntity<JeongsanApiResponse<ExpenseResponse>> getExpensesIPaid(
         Long memberId, Long teamId);
+
+    @Operation(summary = "카테고리 목록 조회 API", description = "지출 카테고리 목록을 조회하는 API")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "카테고리 목록을 성공적으로 조회", content = @Content(schema = @Schema(implementation = CategoryListResponse.class))),
+        @ApiResponse(responseCode = "404", description = "카테고리 목록이 존재하지 않음 (ErrorCode-E404004)")
+    })
+    public ResponseEntity<JeongsanApiResponse<CategoryListResponse>> getCategoryList();
 }
