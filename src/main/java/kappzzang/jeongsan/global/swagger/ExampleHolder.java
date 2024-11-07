715f16a168f41691eabc2b5c462909eb77fa453c
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2024-11-07 12:41:40 +0900

    Feature/issue#128: Swagger에서 같은 코드의 여러 에러 응답을 볼 수 있도록 수정 (#135)
    * Feat: 여러 ErrorType 출력을 위한 Swagger 설정 추가
    
    * Fix: 어노테이션에 원하는 ErrorType을 기입할 수 있도록 수정
    
    * Refactor: 커스텀 어노테이션 사용

---
+++ src/main/java/kappzzang/jeongsan/global/swagger/ExampleHolder.java
@@ -0,0 +1,14 @@
+package kappzzang.jeongsan.global.swagger;
+
+import io.swagger.v3.oas.models.examples.Example;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ExampleHolder {
+
+    private Integer statusCode;
+    private Example holder;
+    private String errorCode;
+}
