68b9d063b40dd6533236a9c28b2df39197dae214
us4c0d3 <us4c0d3@gmail.com>
2024-11-12 00:50:55 +0900

    FIX: Team 도메인이 지연 로딩을 정상적으로 수행하도록 JPQL 수정 및 EntityGraph 추가

--- src/main/java/kappzzang/jeongsan/repository/TeamRepository.java
+++ src/main/java/kappzzang/jeongsan/repository/TeamRepository.java
@@ -2,15 +2,16 @@ package kappzzang.jeongsan.repository;
 
 import java.util.List;
 import kappzzang.jeongsan.domain.Team;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
+    @EntityGraph(attributePaths = {"teamMemberList.member"})
     @Query("SELECT t FROM Team t " +
-        "JOIN FETCH t.teamMemberList tm " +
-        "JOIN FETCH tm.member " +
+        "JOIN t.teamMemberList tm " +
         "WHERE tm.member.id = :memberId " +
         "AND t.isClosed = :isClosed")
     List<Team> findByIsClosed(@Param("memberId") Long memberId, Boolean isClosed);
