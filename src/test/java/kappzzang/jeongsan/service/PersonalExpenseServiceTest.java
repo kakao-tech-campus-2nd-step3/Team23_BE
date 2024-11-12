f316e24f4276d6aa485db420e814636fc827f21b
minju26 <mjalswn26@gmail.com>
2024-11-13 05:00:30 +0900

    Test: 개인 소비 내역 저장/수정 테스트

--- src/test/java/kappzzang/jeongsan/service/PersonalExpenseServiceTest.java
+++ src/test/java/kappzzang/jeongsan/service/PersonalExpenseServiceTest.java
@@ -1,17 +1,15 @@
 package kappzzang.jeongsan.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicReference;
 import kappzzang.jeongsan.domain.Expense;
 import kappzzang.jeongsan.domain.Item;
 import kappzzang.jeongsan.domain.Member;
@@ -20,6 +18,7 @@ import kappzzang.jeongsan.domain.Team;
 import kappzzang.jeongsan.domain.TeamMember;
 import kappzzang.jeongsan.dto.request.SavePersonalExpenseRequest;
 import kappzzang.jeongsan.dto.request.SavePersonalExpenseRequest.ItemInfo;
+import kappzzang.jeongsan.global.common.enumeration.ErrorType;
 import kappzzang.jeongsan.global.exception.JeongsanException;
 import kappzzang.jeongsan.repository.ExpenseRepository;
 import kappzzang.jeongsan.repository.ItemRepository;
@@ -27,339 +26,101 @@ import kappzzang.jeongsan.repository.MemberRepository;
 import kappzzang.jeongsan.repository.PersonalExpenseRepository;
 import kappzzang.jeongsan.repository.TeamMemberRepository;
 import kappzzang.jeongsan.repository.TeamRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestPropertySource(locations = "classpath:application-test.properties")
+@ExtendWith(MockitoExtension.class)
 class PersonalExpenseServiceTest {
 
-    @Autowired
+    private final Long teamId = 1L;
+    private final Long itemId = 1L;
+    private final Long expenseId = 1L;
+    private final Long memberId = 1L;
+    @InjectMocks
     private PersonalExpenseService personalExpenseService;
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
-    @Autowired
+    @Mock
     private TeamRepository teamRepository;
-    @Autowired
+    @Mock
     private ExpenseRepository expenseRepository;
-    @Autowired
+    @Mock
     private ItemRepository itemRepository;
-    @Autowired
+    @Mock
     private PersonalExpenseRepository personalExpenseRepository;
-    @Autowired
+    @Mock
     private TeamMemberRepository teamMemberRepository;
-
-    private Member member1, member2, member3, member4;
-    private Team team;
-    private Expense expense;
-    private Item item1, item2;
-    private PersonalExpense member1_pe;
+    @Mock
+    private Team mockTeam;
+    @Mock
+    private Member mockMember;
+    @Mock
+    private Item mockItem;
+    @Mock
+    private Expense mockExpense;
 
     @BeforeEach
-    void setup() {
-        team = teamRepository.save(new Team("Test Team", "🍎"));
-        member1 = memberRepository.save(
-            Member.builder()
-                .kakaoId("kakaoId1")
-                .email("email1@test.com")
-                .nickname("User1")
-                .build());
-        member2 = memberRepository.save(
-            Member.builder()
-                .kakaoId("kakaoId2")
-                .email("email2@test.com")
-                .nickname("User2")
-                .build());
-        member3 = memberRepository.save(
-            Member.builder()
-                .kakaoId("kakaoId3")
-                .email("email3@test.com")
-                .nickname("User3")
-                .build());
-        member4 = memberRepository.save(
-            Member.builder()
-                .kakaoId("kakaoId4")
-                .email("email4@test.com")
-                .nickname("User4")
-                .build());
-        item1 = new Item("Test Item", 2, 1000);
-        item2 = new Item("Test Item", 3, 1000);
-        expense = Expense.builder()
-            .title("asdf")
-            .category(null)
-            .imageUrl("image.jpg")
-            .team(team)
-            .member(member1)
-            .items(List.of(item1))
-            .paymentTime(LocalDateTime.now())
-            .build();
-        expense = expenseRepository.save(expense);
-        itemRepository.save(item1);
-        itemRepository.save(item2);
-        member1_pe = personalExpenseRepository.save(new PersonalExpense(member1, item2, 1, 1000));
-        teamMemberRepository.save(new TeamMember(member1, team, false, true));
-        teamMemberRepository.save(new TeamMember(member2, team, false, true));
-        teamMemberRepository.save(new TeamMember(member3, team, false, true));
-        teamMemberRepository.save(new TeamMember(member4, team, false, true));
-    }
+    void setUp() {
 
-    @AfterEach
-    void cleanup() {
-        personalExpenseRepository.deleteAll();
-        itemRepository.deleteAll();
-        expenseRepository.deleteAll();
-        teamRepository.deleteAll();
-        memberRepository.deleteAll();
-        teamMemberRepository.deleteAll();
+        given(teamRepository.findById(teamId)).willReturn(Optional.of(mockTeam));
+        given(itemRepository.findById(itemId)).willReturn(Optional.of(mockItem));
+        given(expenseRepository.findById(expenseId)).willReturn(Optional.of(mockExpense));
+        given(memberRepository.findById(memberId)).willReturn(Optional.of(mock(Member.class)));
+        given(teamMemberRepository.findTeamMemberByTeamAndMember(any(), any())).willReturn(
+            Optional.of(mock(TeamMember.class)));
+        given(mockExpense.getTeam()).willReturn(mockTeam);
+        given(mockItem.getQuantity()).willReturn(5);
     }
 
     @Test
-    @DisplayName("3명이 동시에 개인 소비 내역을 저장하면,"
-        + "차례로 처리 되도록 하고, "
-        + "마지막에 처리된 요청이 나머지 금액을 부담한다.")
-    void personalExpenseSaveConcurrencyTest() throws InterruptedException {
-
-        // given
-        int threadCount = 3;
-        List<Member> members = List.of(member1, member2, member3);
-        List<Integer> expectedTotalPrices = List.of(666, 666, 668);
-        CountDownLatch startLatch = new CountDownLatch(1);
-        CountDownLatch endLatch = new CountDownLatch(threadCount);
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-
-        // 마지막에 완료된 memberId 저장 목적
-        AtomicReference<Long> lastCompletedMemberId = new AtomicReference<>();
-
-        // when
-        for (int i = 0; i < threadCount; i++) {
-            Member testMember = members.get(i);
-
-            executorService.submit(() -> {
-                try {
-                    startLatch.await(); // 모든 스레드 대기
-                    SavePersonalExpenseRequest request = new SavePersonalExpenseRequest(
-                        List.of(new SavePersonalExpenseRequest.ItemInfo(item1.getId(), 1)));
-                    personalExpenseService.savePersonalExpense(testMember.getId(), team.getId(),
-                        expense.getId(), request);
-
-                    lastCompletedMemberId.set(testMember.getId());  // 마지막에 완료된 memberId 저장
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    endLatch.countDown();
-                }
-            });
-        }
-
-        startLatch.countDown();
-        endLatch.await();
-        executorService.shutdown();
-
-        // then
-        List<PersonalExpense> savedExpenses = personalExpenseRepository.findAllByItem(item1);
-        List<Integer> actualTotalPrices = savedExpenses.stream().map(PersonalExpense::getTotalPrice)
-            .sorted().toList();
-
-        Long lastUpdatedMemberId = lastCompletedMemberId.get();
-        Integer lastMemberTotalPrice = savedExpenses.stream()
-            .filter(expense -> expense.getMember().getId().equals(lastUpdatedMemberId))
-            .map(PersonalExpense::getTotalPrice).findFirst().orElse(0);
-
-        assertEquals(3, savedExpenses.size());
-        assertTrue(
-            savedExpenses.stream().anyMatch(pe -> pe.getMember().getId().equals(member1.getId())));
-        assertTrue(
-            savedExpenses.stream().anyMatch(pe -> pe.getMember().getId().equals(member2.getId())));
-        assertTrue(
-            savedExpenses.stream().anyMatch(pe -> pe.getMember().getId().equals(member3.getId())));
-
-        assertEquals(expectedTotalPrices, actualTotalPrices);   // 전체 값 검증
-        assertEquals(expectedTotalPrices.get(2), lastMemberTotalPrice); // 마지막에 완료 된 값 668 검증
-    }
-
-    @Test
-    @DisplayName("기존 개인 소비 내역 데이터와 다른 수량으로 요청 시 "
-        + "개인 소비 내역이 업데이트 되고, 관련된 다른 데이터를 업데이트 한다")
-    void updatePersonalExpenseTest() {
-
-        // given
-        personalExpenseRepository.save(new PersonalExpense(member3, item2, 2, 2000));
-        SavePersonalExpenseRequest request = new SavePersonalExpenseRequest(
-            List.of(new SavePersonalExpenseRequest.ItemInfo(item2.getId(), 2)));
-
-        // when
-        personalExpenseService.savePersonalExpense(member1.getId(), team.getId(), expense.getId(),
-            request);
-
-        // then
-        List<PersonalExpense> savedExpenses = personalExpenseRepository.findAllByItem(item2);
-
-        for (PersonalExpense pe : savedExpenses) {
-            assertThat(pe.getQuantity()).isEqualTo(2);
-            assertThat(pe.getTotalPrice()).isEqualTo(1500);
-        }
-    }
-
-    @Test
-    @DisplayName("아이템의 수량보다 많은 수량이 포함된 요청은 예외가 발생한다.")
-    void savePersonalExpenseExceptionTest() {
-
+    @DisplayName("개인 소비 내역을 저장한다.")
+    void savePersonaExpense() {
         // given
         SavePersonalExpenseRequest request = new SavePersonalExpenseRequest(
-            List.of(new ItemInfo(item2.getId(), 10))
-        );
+            List.of(new ItemInfo(itemId, 1)));
+        given(personalExpenseRepository.findByMemberAndItem(any(), any())).willReturn(
+            Optional.empty());
 
-        // when, then
-        assertThrows(JeongsanException.class, () -> {
-            personalExpenseService.savePersonalExpense(member1.getId(), team.getId(),
-                expense.getId(), request);
-        });
+        // when
+        personalExpenseService.saveOrUpdatePersonalExpense(memberId, teamId, expenseId, request);
 
-        List<PersonalExpense> savedExpenses = personalExpenseRepository.findAll();
-        assertEquals(1, savedExpenses.size());
+        // then
+        verify(personalExpenseRepository, times(1)).save(any(PersonalExpense.class));
     }
 
     @Test
-    @DisplayName("기존 개인 소비 내역 데이터와 동일한 수량이 포함된 요청은 예외가 발생한다.")
-    void updatePersonalExpenseWithSameQuantityTest() {
+    @DisplayName("기존의 개인 소비 수량과 같은 수량의 요청이면 업데이트하지 않는다.")
+    void updateWithSameQuantityTest() {
         // given
         SavePersonalExpenseRequest request = new SavePersonalExpenseRequest(
-            List.of(new SavePersonalExpenseRequest.ItemInfo(item2.getId(), 1))
-        );
-
-        // when, then
-        assertThrows(JeongsanException.class, () -> {
-            personalExpenseService.savePersonalExpense(member1.getId(), team.getId(),
-                expense.getId(), request);
-        });
-
-        PersonalExpense savedExpenses = personalExpenseRepository.findByMemberAndItem(member1,
-            item2).get();
-        assertEquals(1, savedExpenses.getQuantity());
-        assertEquals(1000, savedExpenses.getTotalPrice());
-    }
-
-    @Test
-    @DisplayName("개인 소비 내역 수정 요청 시 요청 수량이 0이라면 기존 데이터를 삭제하고,"
-        + " 남은 개인 소비 내역 데이터의 totalPrice 값을 업데이트 한다.")
-    void updatePersonalExpenseWithZeroQuantityTest() {
-        // given
-        member1_pe.update(2, 1500);
-        personalExpenseRepository.save(new PersonalExpense(member3, item2, 2, 1500));
-
-        SavePersonalExpenseRequest updateRequest = new SavePersonalExpenseRequest(
-            List.of(new SavePersonalExpenseRequest.ItemInfo(item2.getId(), 0))
-        );
+            List.of(new ItemInfo(itemId, 1)));
+        PersonalExpense personalExpense = new PersonalExpense(mockMember, mockItem, 1, 1);
+        given(personalExpenseRepository.findByMemberAndItem(any(), any())).willReturn(
+            Optional.of(personalExpense));
 
         // when
-        personalExpenseService.savePersonalExpense(member1.getId(), team.getId(), expense.getId(),
-            updateRequest);
+        personalExpenseService.saveOrUpdatePersonalExpense(memberId, teamId, expenseId, request);
 
         // then
-        Optional<PersonalExpense> deletedPersonalExpense = personalExpenseRepository.findByMemberAndItem(
-            member1, item2);
-        assertTrue(deletedPersonalExpense.isEmpty());
-        Optional<PersonalExpense> updatedPersonalExpense = personalExpenseRepository.findByMemberAndItem(
-            member3, item2);
-        assertThat(updatedPersonalExpense).isNotEmpty();
-        assertThat(updatedPersonalExpense.get().getQuantity()).isEqualTo(2);
-        assertThat(updatedPersonalExpense.get().getTotalPrice()).isEqualTo(2000);
+        verify(personalExpenseRepository, never()).save(any());
     }
 
     @Test
-    @DisplayName("개인 소비 내역 저장 요청 시, 요청 수량이 0이라면 예외가 발생한다.")
-    void savePersonalExpenseWithZeroQuantityTest() {
+    @DisplayName("아이템의 소비 수량보다 큰 수의 수량이 포함된 요청 시 INVALID_QUANTITY 예외가 발생한다")
+    void requestWithInvalidQuantityTest() {
         // given
-        SavePersonalExpenseRequest saveRequest = new SavePersonalExpenseRequest(
-            List.of(new SavePersonalExpenseRequest.ItemInfo(item1.getId(), 0))
-        );
+        SavePersonalExpenseRequest request = new SavePersonalExpenseRequest(
+            List.of(new ItemInfo(itemId, 10)));
 
         // when, then
-        assertThrows(JeongsanException.class, () -> {
-            personalExpenseService.savePersonalExpense(member4.getId(), team.getId(),
-                expense.getId(), saveRequest);
-        });
-
-        Optional<PersonalExpense> personalExpense = personalExpenseRepository.findByMemberAndItem(
-            member4, item1);
-        assertTrue(personalExpense.isEmpty());
-    }
-
-    @Test
-    @DisplayName("개인 소비 내역 저장, 수정 요청이 동시에 요청된다.")
-    void concurrentUpdateAndSaveTest() throws InterruptedException {
-        // given
-        int threadCount = 2;
-        CountDownLatch startLatch = new CountDownLatch(1);
-        CountDownLatch endLatch = new CountDownLatch(threadCount);
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-
-        SavePersonalExpenseRequest updateRequest = new SavePersonalExpenseRequest(
-            List.of(new SavePersonalExpenseRequest.ItemInfo(item2.getId(), 2))
-        );
-        SavePersonalExpenseRequest saveRequest = new SavePersonalExpenseRequest(
-            List.of(new SavePersonalExpenseRequest.ItemInfo(item2.getId(), 2))
-        );
-
-        List<TestCase> testCases = List.of(
-            new TestCase(member1.getId(), "수정", updateRequest),
-            new TestCase(member4.getId(), "저장", saveRequest)
-        );
-
-        // when
-        testCases.forEach(testCase ->
-            executorService.submit(() -> executeTestCase(testCase, startLatch, endLatch))
-        );
-
-        startLatch.countDown();
-        endLatch.await();
-        executorService.shutdown();
-
-        // then
-        List<PersonalExpense> savedExpenses = personalExpenseRepository.findAllByItem(item2);
-        assertEquals(2, savedExpenses.size());
-
-        int totalQuantity = savedExpenses.stream()
-            .mapToInt(PersonalExpense::getQuantity)
-            .sum();
-        assertEquals(4, totalQuantity);  // member1: 2, member4: 2
-
-        int totalPrice = savedExpenses.stream()
-            .mapToInt(PersonalExpense::getTotalPrice)
-            .sum();
-        assertEquals(item2.getTotalPrice(), totalPrice);  // item2의 전체 가격
-
-        for (PersonalExpense personalExpense : savedExpenses) {
-            assertEquals(2, personalExpense.getQuantity());
-            assertEquals(1500, personalExpense.getTotalPrice());
-        }
-    }
-
-    private void executeTestCase(TestCase testCase, CountDownLatch startLatch,
-        CountDownLatch endLatch) {
-        try {
-            startLatch.await();
-            personalExpenseService.savePersonalExpense(
-                testCase.memberId(),
-                team.getId(),
-                expense.getId(),
-                testCase.request()
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            endLatch.countDown();
-        }
-    }
-
-    private record TestCase(Long memberId, String operation, SavePersonalExpenseRequest request) {
-
+        assertThatThrownBy(() ->
+            personalExpenseService.saveOrUpdatePersonalExpense(memberId, teamId, expenseId, request)
+        ).isInstanceOf(JeongsanException.class)
+            .hasMessageContaining(ErrorType.INVALID_QUANTITY.getMessage());
     }
 }
