e7b5d72eb60d38536b034a09fc4394447b9566a0
dkswoals <a4445656@gmail.com>
2024-11-15 03:40:40 +0900

    Refactor: 중복, 의미없는 테스트 코드 제거

--- src/test/java/kappzzang/jeongsan/service/ExpenseServiceTest/UpdateExpensesStateTest.java
+++ src/test/java/kappzzang/jeongsan/service/ExpenseServiceTest/UpdateExpensesStateTest.java
@@ -22,7 +22,6 @@ import kappzzang.jeongsan.repository.ExpenseRepository;
 import kappzzang.jeongsan.service.ExpenseService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -59,128 +58,20 @@ public class UpdateExpensesStateTest {
         completeExpensesRequest = new ChangeExpensesStateRequest(Status.COMPLETED, ids);
     }
 
-    @DisplayName("지출 상태 변경(송금 대기 -> 완료)")
-    @Nested
-    class ChangeExpensesStateComplete {
 
-        @DisplayName("지출 상태 변경 성공")
-        @Test
-        void changeExpensesState_Complete_Success() {
-            //given
-            List<Expense> expenses = createExpenses(TEST_EXPENSES_SIZE);
-            given(mockTeam.getId()).willReturn(TEST_TEAM_ID);
-            given(mockPayer.getId()).willReturn(TEST_MEMBER_ID);
-            expenses.forEach(
-                e -> e.changeStatus(mockTeam.getId(), mockPayer.getId(), Status.PENDING));
-            given(expenseRepository.findAllByIdWithDetails(expenseIds))
-                .willReturn(expenses);
-            //when
-            expenseService.updateExpensesState(completeExpensesRequest, TEST_TEAM_ID,
-                TEST_MEMBER_ID);
-
-            //then
-            assertThat(expenses).extracting(Expense::getStatus).containsOnly(Status.COMPLETED);
-            then(expenseRepository).should()
-                .findAllByIdWithDetails(expenseIds);
-        }
-
-        @DisplayName("지출 상태 변경 실패(이미 완료된 지출 존재)")
-        @Test
-        void completeExpenses_AlreadyCompleted_Fail() {
-            //given
-            List<Expense> expenses = createExpenses(TEST_EXPENSES_SIZE);
-            given(mockTeam.getId()).willReturn(TEST_TEAM_ID);
-            given(mockPayer.getId()).willReturn(TEST_MEMBER_ID);
-            expenses.forEach(
-                e -> e.changeStatus(mockTeam.getId(), mockPayer.getId(), Status.PENDING));
-            given(expenseRepository.findAllByIdWithDetails(expenseIds))
-                .willReturn(expenses);
-            expenses.getFirst().changeStatus(TEST_TEAM_ID, TEST_MEMBER_ID, Status.COMPLETED);
-
-            //when //then
-            assertThatThrownBy(
-                () -> expenseService.updateExpensesState(completeExpensesRequest, TEST_TEAM_ID,
-                    TEST_MEMBER_ID)).isInstanceOf(
-                    JeongsanException.class)
-                .hasMessage(ErrorType.EXPENSE_ALREADY_COMPLETED.getMessage());
-        }
-
-        @DisplayName("지출 상태 변경 실패(존재하지 않는 지출 존재)")
-        @Test
-        void completeExpenses_NotFoundExpense_Fail() {
-            //given
-            List<Expense> expenses = createExpenses(TEST_EXPENSES_SIZE - 1);
-            given(expenseRepository.findAllByIdWithDetails(expenseIds))
-                .willReturn(expenses);
-            //when //then
-            assertThatThrownBy(
-                () -> expenseService.updateExpensesState(completeExpensesRequest, TEST_TEAM_ID,
-                    TEST_MEMBER_ID)).isInstanceOf(
-                    JeongsanException.class)
-                .hasMessage(ErrorType.EXPENSE_NOT_FOUND_ID.getMessage());
-        }
-
-        @DisplayName("지출 상태 변경 실패(아직 진행중인 지출 존재)")
-        @Test
-        void completeExpenses_StatusIsOngoing_Fail() {
-            //given
-            List<Expense> expenses = createExpenses(TEST_EXPENSES_SIZE);
-            given(expenseRepository.findAllByIdWithDetails(expenseIds)).willReturn(expenses);
-            given(mockTeam.getId()).willReturn(TEST_TEAM_ID);
-            given(mockPayer.getId()).willReturn(TEST_MEMBER_ID);
-
-            //when //then
-            assertThatThrownBy(
-                () -> expenseService.updateExpensesState(completeExpensesRequest, TEST_TEAM_ID,
-                    TEST_MEMBER_ID)).isInstanceOf(
-                    JeongsanException.class)
-                .hasMessage(ErrorType.EXPENSE_ONGOING.getMessage());
-        }
-
-        @DisplayName("지출 상태 변경 실패(타 모임의 지출이 포함된 요청)")
-        @Test
-        void completeExpenses_AnotherTeam_Fail() {
-            //given
-            final Long INVALID_TEAM_ID = 2L;
-            List<Expense> expenses = createExpenses(TEST_EXPENSES_SIZE);
-            given(mockPayer.getId()).willReturn(TEST_MEMBER_ID);
-            given(mockTeam.getId()).willReturn(INVALID_TEAM_ID);
-            expenses.forEach(
-                e -> e.changeStatus(mockTeam.getId(), mockPayer.getId(), Status.PENDING));
-            given(expenseRepository.findAllByIdWithDetails(expenseIds))
-                .willReturn(expenses);
-
-            //when //then
-            assertThatThrownBy(
-                () -> expenseService.updateExpensesState(completeExpensesRequest, TEST_TEAM_ID,
-                    TEST_MEMBER_ID)).isInstanceOf(
-                    JeongsanException.class)
-                .hasMessage(ErrorType.EXPENSE_INVALID_TEAM.getMessage());
-        }
-
-
-        @DisplayName("지출 상태 변경 실패(자신이 결제하지 않은 지출이 포함된 요청)")
-        @Test
-        void completeExpenses_AnotherPayer_Fail() {
-            //given
-            final Long INVALID_MEMBER_ID = 2L;
-            List<Expense> expenses = createExpenses(TEST_EXPENSES_SIZE);
-            given(mockPayer.getId()).willReturn(INVALID_MEMBER_ID);
-            given(mockTeam.getId()).willReturn(TEST_TEAM_ID);
-            expenses.forEach(
-                e -> e.changeStatus(mockTeam.getId(), mockPayer.getId(), Status.PENDING));
-            given(expenseRepository.findAllByIdWithDetails(expenseIds))
-                .willReturn(expenses);
-            given(mockTeam.getId()).willReturn(TEST_TEAM_ID);
-            given(mockPayer.getId()).willReturn(INVALID_MEMBER_ID);
-
-            //when //then
-            assertThatThrownBy(
-                () -> expenseService.updateExpensesState(completeExpensesRequest, TEST_TEAM_ID,
-                    TEST_MEMBER_ID)).isInstanceOf(
-                    JeongsanException.class)
-                .hasMessage(ErrorType.EXPENSE_INVALID_PAYER.getMessage());
-        }
+    @DisplayName("사용자가 지출 상태를 송금대기에서 완료로 변경할 때, 존재하지 않는 지출에 대해 요청할 경우, ExpenseNotFoundException을 발생시킨다")
+    @Test
+    void completeExpenses_NotFoundExpense_Fail() {
+        //given
+        List<Expense> expenses = createExpenses(TEST_EXPENSES_SIZE - 1);
+        given(expenseRepository.findAllByIdWithDetails(expenseIds))
+            .willReturn(expenses);
+        //when //then
+        assertThatThrownBy(
+            () -> expenseService.updateExpensesState(completeExpensesRequest, TEST_TEAM_ID,
+                TEST_MEMBER_ID)).isInstanceOf(
+                JeongsanException.class)
+            .hasMessage(ErrorType.EXPENSE_NOT_FOUND_ID.getMessage());
     }
 
 
