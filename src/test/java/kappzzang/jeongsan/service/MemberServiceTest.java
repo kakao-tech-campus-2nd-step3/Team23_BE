26c535963a805da483e11161cff14ac03e2c5ff6
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2024-11-15 09:52:33 +0900

    Test/issue#176: 회원가입, 로그인 테스트 리팩토링 및 통합 테스트 추가 (#201)
    * Chore: 멤버 서비스 테스트 이름 수정
    
    * Test: 회원 관련 통합 테스트 추가
    
    * Chore: 테스트 이름 오타 수정

--- src/test/java/kappzzang/jeongsan/service/MemberServiceTest.java
+++ src/test/java/kappzzang/jeongsan/service/MemberServiceTest.java
@@ -51,7 +51,7 @@ public class MemberServiceTest {
     private MemberService memberService;
 
     @Test
-    @DisplayName("로그인 실패 - 회원가입 필요")
+    @DisplayName("회원가입을 하지 않고 로그인하면 예외가 발생한다.")
     void loginWithoutRegistration() {
         // given
         given(memberRepository.findByEmail(anyString())).willReturn(Optional.empty());
@@ -63,7 +63,7 @@ public class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("로그인 성공")
+    @DisplayName("로그인하면 서비스 토큰을 반환한다.")
     void login() {
         // given
         given(memberRepository.findByEmail(anyString())).willReturn(
@@ -82,7 +82,7 @@ public class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("회원가입 실패 - 이미 회원가입됨")
+    @DisplayName("회원가입 후 다시 회원가입하면 예외가 발생한다.")
     void registerAfterRegistration() {
         // given
         RegisterRequest registerRequest = new RegisterRequest(TEST_UUID, TEST_NICKNAME, TEST_EMAIL,
@@ -97,7 +97,7 @@ public class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("회원가입 성공")
+    @DisplayName("회원가입하면 서비스 토큰을 반환한다.")
     void register() {
         // given
         RegisterRequest registerRequest = new RegisterRequest(TEST_UUID, TEST_NICKNAME, TEST_EMAIL,
@@ -118,7 +118,7 @@ public class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("액세스 토큰 재발급 실패 - 일치하지 않은 리프레시 토큰")
+    @DisplayName("발급받은 리프레시 토큰과 다른 리프레시 토큰으로 액세스 토큰을 요청하면 예외가 발생한다.")
     void refreshWithMismatchedRefreshToken() {
         // given
         RefreshRequest refreshRequest = new RefreshRequest("RefreshToken");
@@ -131,7 +131,7 @@ public class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("액세스 토큰 재발급 실패 - 유효하지 않은 리프레시 토큰")
+    @DisplayName("유효하지 않은 리프레시 토큰으로 액세스 토큰을 요청하면 예외가 발생한다.")
     void refreshWithInvalidRefreshToken() {
         // given
         RefreshRequest refreshRequest = new RefreshRequest(TEST_REFRESH_TOKEN);
@@ -145,7 +145,7 @@ public class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("액세스 토큰 재발급 성공")
+    @DisplayName("유효한 리프레시 토큰으로 액세스 토큰을 요청하면 재발급된 액세스 토큰을 반환한다.")
     void refresh() {
         // given
         RefreshRequest refreshRequest = new RefreshRequest(TEST_REFRESH_TOKEN);
@@ -162,7 +162,7 @@ public class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("카카오 페이 송금 링크 조회 테스트")
+    @DisplayName("카카오 페이 송금 링크가 등록되어 있을 때 조회하면 카카오 페이 송금 링크를 반환한다.")
     void getPayLink() {
         // given
         KakaoPayInfo kakaoPayInfo = new KakaoPayInfo("payLink");
@@ -177,7 +177,7 @@ public class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("카카오 페이 송금 링크 조회 테스트 - 페이 링크가 null인 경우")
+    @DisplayName("카카오 페이 송금 링크를 등록하지 않고 조회하면 예외가 발생한다.")
     void getPayLinkException() {
         // given
         KakaoPayInfo kakaoPayInfo = new KakaoPayInfo(null);
